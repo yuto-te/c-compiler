7c1cb61c55d0b198134e52e62d6e72b7918e8c56
Yuto Terauchi <yutoterauchi0304@gmail.com>
2019-07-18 20:52:50 +0900

    1文字のローカル変数の導入

--- 1cc/1cc.h
+++ 1cc/1cc.h
@@ -8,6 +8,7 @@
 // トークンの種類
 typedef enum {
   TK_RESERVED, // 記号
+  TK_IDENT,    // 識別子
   TK_NUM,      // 整数トークン
   TK_EOF,      // 入力の終わりを表すトークン
 } TokenKind;
@@ -15,39 +16,45 @@ typedef enum {
 typedef struct Token Token;
 
 // トークン型
-typedef struct Token {
+struct Token {
   TokenKind kind; // トークンの型
   Token *next;    // 次の入力トークン
   int val;        // kindがTK_NUMの場合，その数値
   char *str;      // トークン文字列
   int len         // トークンの長さ
-} Token;
+};
 
 // 抽象構文木のノードの種類
 typedef enum {
-  ND_ADD, // +
-  ND_SUB, // -
-  ND_MUL, // *
-  ND_DIV, // /
-  ND_EQ,  // ==
-  ND_NEQ, // !=
-  ND_ELT, // <=
-  ND_LT,  // <
-  ND_NUM, // 整数
+  ND_ADD,    // +
+  ND_SUB,    // -
+  ND_MUL,    // *
+  ND_DIV,    // /
+  ND_ASSIGN, // =
+  ND_LVAR,   // ローカル変数
+  ND_EQ,     // ==
+  ND_NEQ,    // !=
+  ND_ELT,    // <=
+  ND_LT,     // <
+  ND_NUM,    // 整数
 } NodeKind;
 
 typedef struct Node Node;
 
 // 抽象構文木のノードの型
-typedef struct Node {
+struct Node {
   NodeKind kind; // ノードの型
   Node *lhs;     // 左辺
   Node *rhs;     // 右辺
   int val;       // kindがND_NUMの場合のみ使う
-} Node;
+  int offset;    // kindがND_LVARの場合のみ使う
+};
 
 // 現在着目しているトークン
 Token *token;
+
+// program
+Node *code[100];
 
 // parse.c
 // トークナイズ
@@ -64,7 +71,10 @@ void expect(char *op);
 int expect_number();
 bool at_eof();
 
+Node *program();
+Node *stmt();
 Node *expr();
+Node *assign();
 Node *equality();
 Node *relational();
 Node *add();
@@ -74,4 +84,5 @@ Node *term();
 
 // codegen.c
 // コード生成
+void gen_lval(Node *node);
 void gen(Node *node);
